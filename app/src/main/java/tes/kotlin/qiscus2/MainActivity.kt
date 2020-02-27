c06dbe146ce6e222cfbe4d02dd391fc090679f6b
bimtut <masbimtut@gmail.com>
2020-02-27 16:57:14 +0700

    2nd step

--- app/src/main/java/tes/kotlin/qiscus2/MainActivity.kt
+++ app/src/main/java/tes/kotlin/qiscus2/MainActivity.kt
@@ -10,13 +10,18 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-//        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-//
-//        val dataList = ArrayList<Model>()
-//        dataList.add(Model("Phone", 1))
-//        dataList.add(Model("Watch", 2))
-//        dataList.add(Model("Note", 3))
-//        dataList.add(Model("Pin", 4))
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
+
+        val dataList = ArrayList<Model>()
+        dataList.add(Model("Phone", 1))
+        dataList.add(Model("Watch", 2))
+        dataList.add(Model("Note", 3))
+        dataList.add(Model("Pin", 4))
+//        pass the values to RvAdapter
+        val rvAdapter = RecyclerViewAdapter(dataList)
+//        set the recyclerView to the adapter
+        recyclerView.adapter = rvAdapter;
+
     }
 }
